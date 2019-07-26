d1c8964da31482ae2e6146c8160d3eeed5a0943c
Antonio Mendes <antonio.mendes.contador@gmail.com>
2019-07-25 22:19:16 -0400

    Produto e associacao muitos para muitos

--- src/main/java/com/mendes/cursomc/repositorys/ProdutoRepository.java
+++ src/main/java/com/mendes/cursomc/repositorys/ProdutoRepository.java
@@ -3,9 +3,9 @@ package com.mendes.cursomc.repositorys;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.mendes.cursomc.domain.Categoria;
+import com.mendes.cursomc.domain.Produto;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 
 }
